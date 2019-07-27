ab4e208679e3550515b197009e190fdc6fcc3245
Erokate <45769864+TurkErenTr@users.noreply.github.com>
2019-07-27 07:09:39 +0300

    Update ready.js
    4 değişiklik

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("streaming");
   function changing_status() {
-    let status = ["Hüsam Bot Tekrardan Aktif! 🎉", "İnstagram: @kushusam", "🎉 6 Yeni Komut: Bu Komutları Öğrenmek İçin *yk yaz. 🎉", `${ayarlar.prefix}yardım ya da ${ayarlar.prefix}help Yazarak Komut Listesine Ulaşabilirsin!`, "🎉 Hüsam Bot Discord Sunucusuna Katılmak İçin discord.io/HUSAM 🎉", "🎉 Erokate Discord Sunucusuna Katılmak İçin discord.io/EROKATE 🎉", `🎉 ${ayarlar.prefix}swerctr Komutu Güncellendi!🎉`, `🎉 SwercTR Discord Botu Yapımda ${ayarlar.prefix}swerctr Yazıp Sunucuya Gelmeyi Unutmayın! 🎉`, `✨Artık Botumuz 7/24 Aktif!!✨`, `🎉Artık Botumuz 7/24 Aktif!!🎉`, `🎊Artık Botumuz 7/24 Aktif!!🎊`, `🎈Artık Botumuz 7/24 Aktif!!🎈`, ]
+    let status = ["Hüsam Bot Tekrardan Aktif! 🎉", "İnstagram: @kushusam", "🎉 6 Yeni Komut: Bu Komutları Öğrenmek İçin *yk yaz. 🎉", `${ayarlar.prefix}yardım ya da ${ayarlar.prefix}help Yazarak Komut Listesine Ulaşabilirsin!`, "🎉 Hüsam Bot Discord Sunucusuna Katılmak İçin discord.io/HUSAM 🎉", "🎉 Erokate Discord Sunucusuna Katılmak İçin discord.io/EROKATE 🎉", `🎉 ${ayarlar.prefix}swerctr Komutu Güncellendi!🎉`, `🎉 SwercTR Discord Botu Yapımda ${ayarlar.prefix}swerctr Yazıp Sunucuya Gelmeyi Unutmayın! 🎉`, `✨Artık Botumuz 7/24 Aktif!!✨`, `🎈Artık Botumuz 7/24 Aktif!!🎈`, ]
     let randomStatus = status[Math.floor(Math.random() * status.length)]
     client.user.setActivity(randomStatus, {type: 'STREAMING'});
   }
